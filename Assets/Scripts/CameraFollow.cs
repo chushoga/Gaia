760703dbacafecffb879e933c09377665606c698
chushoga <chushoga@gmail.com>
2018-02-18 22:59:56 +0900

    update

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject player;
+	private float cameraHeight = 6.0f;
+	private float cameraOffset = -6.0f;
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = player.transform.position;
+		pos.y += cameraHeight;
+		pos.z += cameraOffset;
+		pos.x += cameraOffset;
+		transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+	
+	Camera cam;
+
+	public LayerMask groundLayer;
+	public NavMeshAgent playerAgent;
+
+	void Awake(){
+		cam = Camera.main;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			playerAgent.SetDestination(GetPointUnderCursor());
+		}
+	}
+
+	private Vector3 GetPointUnderCursor(){
+		Vector2 screenPosition = Input.mousePosition;
+		Vector3 mouseWorldPosition = cam.ScreenToWorldPoint(screenPosition);
+
+		RaycastHit hitPosition;
+
+		Physics.Raycast(mouseWorldPosition, cam.transform.forward, out hitPosition, 100, groundLayer);
+
+		return hitPosition.point;
+	}
+}
